8b37267ab593fd6d06f33b5f92a5835d50c128ed
yettyola <yettyolukemi@gmail.com>
2023-06-29 21:40:34 +0000

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+cat 1-main.js
+import initializeRooms from './1-make_classrooms.js';
+console.log(initializeRooms());
+npm run dev 1-main.js
+[
+  ClassRoom { _maxStudentsSize: 19 },
+  ClassRoom { _maxStudentsSize: 20 },
+  ClassRoom { _maxStudentsSize: 34 }
+]
